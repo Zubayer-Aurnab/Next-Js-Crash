2e241758825315bbee548c4fc849ba768796ba27
Aurnab <zubayeraurnab@gmail.com>
2024-01-07 17:46:01 +0600

    Page & Layout

---
+++ app/about/mission/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Mission = () => {
+    return (
+        <div className='mt-10'>
+            <h1>Mission</h1>
+        </div>
+    );
+};
+
+export default Mission;
--- app/page.js
+++ app/page.js
@@ -1,8 +1,8 @@
 
 export default function Home() {
   return (
-    <main>
-      <h1>Hello World</h1>
+    <main className="mt-10">
+      <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus molestiae officia minus provident? Vitae molestiae optio cum expedita illo nam nihil earum minima numquam repudiandae? Praesentium, recusandae. Accusantium, enim omnis!</h1>
     </main>
   );
 }
