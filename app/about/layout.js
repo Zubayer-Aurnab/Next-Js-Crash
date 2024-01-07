f49dcec7ee8cad73e89443db13f6356bf4aa4825
Aurnab <zubayeraurnab@gmail.com>
2024-01-07 18:04:15 +0600

    Routing & Navigation

--- app/about/layout.js
+++ app/about/layout.js
@@ -1,9 +1,19 @@
+import Link from "next/link";
 import React from "react";
 
 const AboutLayout = ({ children }) => {
   return (
     <div>
-      <nav className="my-6">Mission ||Vision</nav>
+      <nav className="mb-2">
+          <ul className="flex gap-6 mt-10 border-b">
+            <li>
+              <Link href="/about/mission">Mission</Link>
+            </li>
+            <li>
+              <Link href="/about/vision">Vision</Link>
+            </li>
+          </ul>
+        </nav>
       {children}
     </div>
   );
--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,7 +13,16 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className="p-6">
       <body className={inter.className}>
-        <nav className="mb-2">Home || About</nav>
+        <nav className="mb-2">
+          <ul className="flex gap-6">
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
         <hr />
         {children}
       </body>
