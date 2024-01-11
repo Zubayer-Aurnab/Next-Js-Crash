2fa98739e3620bae6ad1f7b02a4a8a8b2c9126b8
Aurnab <zubayeraurnab@gmail.com>
2024-01-11 21:25:32 +0600

    Not-Found Page

--- app/blogs/[id]/page.js
+++ app/blogs/[id]/page.js
@@ -1,10 +1,16 @@
+import { notFound } from "next/navigation";
 import React from "react";
 
 const BlogPage = ({ params }) => {
-  console.log(params);
-  return <div>
-    <h1 className="mt-10">the Blog id is : {params.id}</h1>
-  </div>;
+  //   console.log(params);
+  if (params.id === "4") {
+    notFound();
+  }
+  return (
+    <div>
+      <h1 className="mt-10">the Blog id is : {params.id}</h1>
+    </div>
+  );
 };
 
 export default BlogPage;
