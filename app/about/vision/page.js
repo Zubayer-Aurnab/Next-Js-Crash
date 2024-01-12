99e15996f188f23933934c3de465d46ab665590f
Aurnab <zubayeraurnab@gmail.com>
2024-01-12 19:42:32 +0600

    Image Optimization

--- app/about/vision/page.js
+++ app/about/vision/page.js
@@ -1,11 +1,12 @@
-
-
+import Image from "next/image";
+import titne from "@/public/EREN_TITNE_.jpg"
 const page = () => {
-    return (
-        <div>
-            <h1>Vision Page</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Vision Page</h1>
+      <Image placeholder="blur" src={titne} alt="titne" quality={100}/>
+    </div>
+  );
 };
 
 export default page;
