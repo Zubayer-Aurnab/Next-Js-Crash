e0a61bfcfe5c769b851caee911af4ee29de77a58
Aurnab <zubayeraurnab@gmail.com>
2024-01-12 20:01:16 +0600

    Font Optimization

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,12 @@
-import { Inter } from "next/font/google";
+import { Roboto } from 'next/font/google'
 import "./globals.css";
 import Link from "next/link";
 
-const inter = Inter({ subsets: ["latin"] });
+const roboto = Roboto({
+  weight: '400',
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 export const metadata = {
   title: "Create Next App",
@@ -12,7 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="p-6">
-      <body className={inter.className}>
+      <body className={roboto.className}>
         <nav className="mb-2">
           <ul className="flex gap-6">
             <li>
