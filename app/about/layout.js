3942755a183bb7de62632c9ceb2e262844bc4ffa
Aurnab <zubayeraurnab@gmail.com>
2024-01-15 12:21:50 +0600

    Static Metadata

--- app/about/layout.js
+++ app/about/layout.js
@@ -1,19 +1,24 @@
 import Link from "next/link";
 import React from "react";
 
+export const metadata = {
+  title: "About Us",
+  description: "this is Blog about page",
+};
+
 const AboutLayout = ({ children }) => {
   return (
     <div>
       <nav className="mb-2">
-          <ul className="flex gap-6 mt-10 border-b">
-            <li>
-              <Link href="/about/mission">Mission</Link>
-            </li>
-            <li>
-              <Link href="/about/vision">Vision</Link>
-            </li>
-          </ul>
-        </nav>
+        <ul className="flex gap-6 mt-10 border-b">
+          <li>
+            <Link href="/about/mission">Mission</Link>
+          </li>
+          <li>
+            <Link href="/about/vision">Vision</Link>
+          </li>
+        </ul>
+      </nav>
       {children}
     </div>
   );
